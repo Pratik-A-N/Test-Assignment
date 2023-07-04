645f025e065bbe9dae12fd57c2cc0a9c29cbb4d0
Pratik Nimje <91190547+Pratik-A-N@users.noreply.github.com>
2023-07-05 02:24:01 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
